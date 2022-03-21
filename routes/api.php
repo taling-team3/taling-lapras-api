08df7993c394d4e313b21351727a27a43a4eb8df
Kyungseo-Park <troublesome.dev@gmail.com>
2022-03-21 22:17:02 +0900

    feat(Test-API): add API router for testing in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('test', [App\Http\Controllers\TestController::class, 'index']);
+Route::post('test', [App\Http\Controllers\TestController::class, 'create']);

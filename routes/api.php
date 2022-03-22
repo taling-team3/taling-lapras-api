5b0e19749f0f933345b9e5633f32c18dedf10e97
Kyungseo-Park <troublesome.dev@gmail.com>
2022-03-23 02:19:17 +0900

    feat(home):  add recommend && taling-best

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ Route::post('test', [App\Http\Controllers\TestController::class, 'create']);
 
 Route::get('thumbnails', [\App\Http\Controllers\V1\HomeController::class, 'getThumbnails']);
 Route::get('categories', [\App\Http\Controllers\V1\HomeController::class, 'getCategories']);
+Route::get('taling-recommend', [\App\Http\Controllers\V1\HomeController::class, 'getRecommend']);
+Route::get('taling-best', [\App\Http\Controllers\V1\HomeController::class, 'getTalingBest']);

b81cbebf844de15218820892b126adab7a87c266
Kyungseo-Park <troublesome.dev@gmail.com>
2022-03-23 01:19:26 +0900

    feat(home): add thumbnail api

---
+++ app/Http/Controllers/V1/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Traits\ApiResponseTrait;
+use App\Models\Thumbnail;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HomeController extends Controller
+{
+    use ApiResponseTrait;
+
+    public function getThumbnails()
+    {
+        try {
+            $thumbnails = Thumbnail::where('is_show', true)->orderBy('is_order', 'asc')->get();
+            return $this->success('썸네일을 불러 왔습니다.', new JsonResource($thumbnails));
+        } catch (\Exception $err) {
+            return $this->error($err->getMessage(), $err->getCode());
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('test', [App\Http\Controllers\TestController::class, 'index']);
 Route::post('test', [App\Http\Controllers\TestController::class, 'create']);
+
+Route::get('thumbnails', [\App\Http\Controllers\V1\HomeController::class, 'getThumbnails']);
